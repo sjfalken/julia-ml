3513277bca39313bd7f81aaf87f58ece692a486f
Stefan Falkenstein <sjfalken@gmail.com>
2023-05-28 15:56:37 +0000

    working now

--- conv.jl
+++ conv.jl
@@ -1,11 +1,11 @@
 using Pkg
 Pkg.activate(".")
 # Add the required packages to the environment
-Pkg.add(["Images", "Flux", "MLDatasets", "CUDA", "Parameters", "ProgressMeter"])
+# Pkg.add(["Images", "Flux", "MLDatasets", "CUDA", "Parameters", "ProgressMeter"])
 using Base.Iterators: partition
 using Flux
 using Flux.Optimise: update!, train!
-using Flux.Losses: logitbinarycrossentropy
+using Flux.Losses: logitcrossentropy
 using Images
 using MLDatasets
 using Statistics
@@ -13,7 +13,7 @@ using Printf
 using Random
 using CUDA
 using ProgressMeter: @showprogress
-
+using JLD2
 Base.@kwdef struct HyperParams
     batch_size::Int = 128
     epochs::Int = 40
@@ -23,20 +23,16 @@ end
 
 hparams = HyperParams()
 
+train_dataset = MLDatasets.MNIST(:train)
+test_dataset = MLDatasets.MNIST(:test)
 
-dataset = MLDatasets.MNIST(:train)
-dataset_test = MLDatasets.MNIST(:test)
+function loader(data::MNIST; batchsize=hparams.batch_size)
+    x4dim = reshape(data.features, 28,28,1,:)   # insert trivial channel dim
+    yhot = Flux.onehotbatch(data.targets, 0:9)  # make a 10×60000 OneHotMatrix
+    Flux.DataLoader((x4dim, yhot); batchsize=batchsize, shuffle=true) |> gpu
+    # Dataloader()
+end
 
-images = dataset.features
-labels = dataset.targets
-
-indices = findall(val-> val < 2, labels)
-images = images[:, :, indices]
-labels = labels[indices]
-
-indices = findall(val-> val < 2, dataset_test.targets)
-images_test = dataset_test.features[:, :, indices]
-labels_test = dataset_test.targets[indices]
 randinit(shape...) = randn(Float32, shape...)
 function Convolutional()
     return Chain(
@@ -49,28 +45,28 @@ function Convolutional()
     )
 end
 convlayers = Convolutional()
-dummy_input = reshape(images, 28, 28, 1, :)
+dummy_input = reshape(train_dataset.features, 28, 28, 1, :)
 dummy_output = convlayers(dummy_input[:, :, :, 1:1])
 
 function EndLayers()
     osize = reduce(*, size(dummy_output)[1:3])
     return Chain(
         x->reshape(x, osize, :),
-        Dense(osize, 1),
+        Dense(osize, 10),
     )
 end
 
-# ConvModel = Chain(Convolutional(), EndLayers()) |> gpu
-convmodel = lenet = Chain(
-    Conv((5, 5), 1=>6, relu),
-    MaxPool((2, 2)),
-    Conv((5, 5), 6=>16, relu),
-    MaxPool((2, 2)),
-    Flux.flatten,
-    Dense(256 => 120, relu),
-    Dense(120 => 84, relu), 
-    Dense(84 => 10),
-) |> gpu
+convmodel = Chain(Convolutional(), EndLayers()) |> gpu
+# convmodel = lenet = Chain(
+#     Conv((5, 5), 1=>6, relu),
+#     MaxPool((2, 2)),
+#     Conv((5, 5), 6=>16, relu),
+#     MaxPool((2, 2)),
+#     Flux.flatten,
+#     Dense(256 => 120, relu),
+#     Dense(120 => 84, relu), 
+#     Dense(84 => 10),
+# ) |> gpu
 
 function getloss(model, data, label)
     
@@ -80,52 +76,42 @@ function getloss(model, data, label)
     # zero = logitbinarycrossentropy(output[findall(x->x==0, label)], 0)
 
 
-    return logitbinarycrossentropy(output[1], label)
+    return logitcrossentropy(output, label)
 end
 
 
 
 
-
-function do_testing(test_data)
-    # image_tensor = reshape(@.(2f0 * test_data - 1f0), 28, 28, 1, :)
-    correct = 0
-    total = 0
-    for (data, label) in test_data
-        # @info label
-        # data = image_tensor[:, :, :, x:x]
-        # label = labels_test[x:x]
-        inference = convmodel(data) |> cpu
-        for x in 1:size(inference, 2)
-            # @info inference[1,x]
-            pred = sigmoid(inference[1, x])
-            if (round(pred) == label[x])
-                correct += 1
-            end
-            total += 1
-
-        end
-
-
-        # data = reshape(@.(2f0* images_test[]))
-    end
-    
-    @info "correct: $correct / $total"
-end
 function do_training()
-    image_tensor = reshape(images, 28, 28, 1, :)
-    test_image_tensor = reshape(images_test, 28, 28, 1, :)
+    # image_tensor = reshape(images, 28, 28, 1, :)
+    # test_image_tensor = reshape(images_test, 28, 28, 1, :)
     # labels = reshape(labels, 1, :)
     # label_tensor = reshape(labels, )
 
-    data = [(image_tensor[:, :, :, r] |> gpu, labels[r]) for r in partition(1:size(image_tensor, 4), hparams.batch_size)]
-    test_data = [(test_image_tensor[:, :, :, r] |> gpu, labels_test[r]) for r in partition(1:size(test_image_tensor, 4), hparams.batch_size)]
+    # data = [(image_tensor[:, :, :, r] |> gpu, labels[r]) for r in partition(1:size(image_tensor, 4), hparams.batch_size)]
+    # test_data = [(test_image_tensor[:, :, :, r] |> gpu, labels_test[r]) for r in partition(1:size(test_image_tensor, 4), hparams.batch_size)]
     opt = Flux.setup(Adam(hparams.lr), convmodel)
 
+    function mycb()
+        (x,y) = only(loader(test_dataset; batchsize=length(test_dataset)))  # make one big batch
+        ŷ = convmodel(x)
+        loss = Flux.logitcrossentropy(ŷ, y)  # did not include softmax in the model
+        acc = round(100 * mean(Flux.onecold(ŷ) .== Flux.onecold(y)); digits=2)
+        # (; loss, acc, split=data.split)  # return a NamedTuple
+
+        @info "Loss: $loss; accuracy: $acc"
+    end
+    cb = Flux.throttle(mycb, 10)
     for ep in 1:hparams.epochs
         @info "Epoch $ep"
-        train!(getloss, convmodel, data, opt)
-        do_testing(test_data)
+        @showprogress for d in loader(train_dataset)
+            ∂L∂m = gradient(getloss, convmodel, d...)[1]
+            update!(opt, convmodel, ∂L∂m)
+        end
+        cb()
+        # train!(getloss, convmodel, loader(train_dataset), opt, cb = Flux.throttle(mycb, 5))
+        # train!
+        # do_testing(test_data)
     end
 end
 do_training()

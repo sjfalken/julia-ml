15c9d7d76d58c3c85d0432f48dcd1e91087d5b3c
Stefan Falkenstein <sjfalken@gmail.com>
2023-09-17 11:07:09 +0000

    tb

--- autoencoder.jl
+++ autoencoder.jl
@@ -22,11 +22,17 @@ data = DataLoader(image_tensor |> gpu, batchsize = 16)
 
 encoder = Chain(
     flatten,
-    Dense(28*28 => 4),   # activation function inside layer
+    Dense(28*28 => 256), 
+    x -> tanh.(x),
+    Dense(256 => 8), 
+    x -> tanh.(x),
 )
 
 decoder = Chain(
-    Dense(4 => 28*28),   # activation function inside layer
+    Dense(8 => 256), 
+    x -> tanh.(x),
+    Dense(256 => 28*28), 
+    x -> tanh.(x),
     x -> reshape(x, 28, 28, :),
 )
 
@@ -43,22 +49,29 @@ tblg = TBLogger("tb_logs/run")
 optim = setup(Adam(), model)  # will store optimiser momentum, etc.
 losses = []
 
-function logtbloss(epoch, loss)
+
+steps = 0
+last_steps_loss = 0
+last_steps_img = 0
+
+function logtbloss(epoch, loss, steps)
     with_logger(tblg) do
-        @info "" epoch loss
+        @info "" epoch loss log_step_increment=(steps - last_steps_loss)
+        global last_steps_loss = steps
         # @info "progress" img
     end
 end
 
-function logtbimg(img)
+function logtbimg(img, steps)
     with_logger(tblg) do
         # @info "" epoch loss
-        @info "progress" img
+        @info "progress" img log_step_increment=(steps - last_steps_img)
+        global last_steps_img = steps
     end
 end
 
-it_cb = throttle((e, l) -> logtbloss(e, l), 1)
-ep_cb = throttle((i) -> logtbimg(i), 5)
+it_cb = throttle((e, l, s) -> logtbloss(e, l, s), 1)
+ep_cb = throttle((i, s) -> logtbimg(i, s), 5)
 
 sample_data = first(data)[:, :, 1:2]
 
@@ -76,15 +89,17 @@ for epoch in 1:100
 
 
 
-        it_cb(epoch, lossval)
+        it_cb(epoch, lossval, steps)
         
         # push!(losses, lossval)
         # dotblog(epoch, val)
         update!(optim, model, grads[1])
 
+        global steps += 1
+
     end
     img = jim(cat([sample_data |> cpu, model(sample_data) |> cpu]; dims=3))
-    ep_cb(img)
+    ep_cb(img, steps)
 
     # plot(losses)
     # savefig("output/plot.png")
